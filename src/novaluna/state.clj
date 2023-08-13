49e94bbcb9c7ce053668ca12153339d813e8be12
alphajuliet <andrew@alphajuliet.com>
2023-08-13 17:24:53 +1000

    Refactor analysis functions into action.clj

--- src/novaluna/state.clj
+++ src/novaluna/state.clj
@@ -1,7 +1,5 @@
 (ns novaluna.state
-  (:require [instaparse.core :as insta]
-            [novaluna.tiles :as tile]
-            [clojure.edn :as edn]))
+  (:require [novaluna.tiles :as tile]))
 
 ;;---------------------------
 (defn map-kv
@@ -26,27 +24,6 @@
 ;;---------------------------
 
 (def init-board {})
-
-(defn find-connected-color
-  "Find connected tiles of the given colour"
-  [board x y colour visited]
-  (let [current-pos [x y]
-        current-tile (get board current-pos)]
-    (if (or (contains? visited current-pos)
-            (nil? current-tile)
-            (not= (:colour current-tile) colour))
-      0
-      (let [new-visited (conj visited current-pos)
-            neighbours (filter #(contains? board %) [[(inc x) y] [(dec x) y] [x (inc y)] [x (dec y)]])]
-        (inc (reduce +
-                  (map #(find-connected-color board (first %) (second %) colour new-visited)
-                       neighbours)))))))
-
-(defn count-connected-color
-  "Count adjacent tiles with the same colour"
-  [board x y]
-  (let [tile (get board [x y])]
-    (find-connected-color board x y (:colour tile) #{})))
 
 (defn init-test-state
   "Initial board state for testing"
